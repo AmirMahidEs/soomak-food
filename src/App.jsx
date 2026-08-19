4b594f47a2f98e18ae2e49c37a5bf39c2e7ae046
Amir Mahdi <amirmahdi1320es@gmail.com>
2026-08-19 17:45:40 +0330

    Refactor App component for improved code organization and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { AnimatePresence } from "framer-motion";
+import { useLayoutEffect } from "react";
 import { useLocation, useRoutes } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+
 export default function App() {
   const location = useLocation();
+
   const element = useRoutes([
     { path: "/", element: <HomePage /> },
     { path: "/products", element: <ProductsPage /> },
     { path: "/products/:id", element: <ProductDetailsPage /> },
     { path: "*", element: <NotFoundPage /> },
   ]);
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <Layout>
       <AnimatePresence mode="wait">
